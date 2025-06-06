dceb7d71a20274fd87ffc95fe5977250cdbd9bb5
a256b <arielb02965@gmail.com>
2025-06-06 17:32:03 -0300

    ui: active fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -170,7 +170,7 @@
                 <div class="position-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="sidebar-link active" href="{{ route('perfumes.index') }}">
+                        <a class="sidebar-link {{ request()->routeIs('perfumes.*') ? 'active' : '' }}" href="{{ route('perfumes.index') }}">
                                 <i class="fas fa-spray-can me-2"></i>Perfumes
                             </a>
                         </li>
